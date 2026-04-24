5ab0c85570b8a3b681ffa5bf10724c98dba20e7a
francois parascan <francois.parascan@gmail.com>
2026-04-24 16:19:39 +0200

    enemi ok et controller

--- Assets/_/code/PlayerMover.cs
+++ Assets/_/code/PlayerMover.cs
@@ -3,10 +3,16 @@ using UnityEngine;
 public class PlayerMover : MonoBehaviour
 {
     public float moveSpeed = 5f;
+    public float m_deadZoneTolerence = 0.1f;
 
     public void Move(Vector2 input)
     {
-        Vector3 delta = new Vector3(input.x, 0f, input.y) * moveSpeed * Time.deltaTime;
+        Vector3 currentFrameInput = new Vector3(input.x, 0f, input.y);
+        if (currentFrameInput.magnitude < m_deadZoneTolerence) return;
+
+        Vector3 delta = currentFrameInput  * moveSpeed * Time.deltaTime;
         transform.position += delta;
+
+
     }
 }
